1273dfed3e85694717b7b65e508558a972ef3dcb
Chukwuma Nwaugha <nwaughac@gmail.com>
2023-10-18 16:24:34 -0700

    expose db data in root endpoint

--- index.js
+++ index.js
@@ -8,6 +8,7 @@ const PORT = process.env.PORT || 3000;
 
 server.use(middleware);
 
+server.get("/", (_req, res) => res.jsonp(router.db.value()));
 server.use(router);
 server.listen(PORT, () => {
   console.info(`Port:${PORT}`);
