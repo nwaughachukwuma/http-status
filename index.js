6bf42af4f4253a08f6b6e87bd51c330d28e397ba
Chukwuma Nwaugha <nwaughac@gmail.com>
2023-10-18 16:29:13 -0700

    override router.render with custom implementation

--- index.js
+++ index.js
@@ -8,6 +8,21 @@ const PORT = process.env.PORT || 3000;
 
 server.use(middleware);
 
+/** @type {jsonServer.JsonServerRouter} */
+router.render = (req, res) => {
+  const code = req.path.match(/^\/(\d+)/)?.[1];
+
+  if (!code) {
+    res.status(400).jsonp("Bad Request. Use '/api/{status_code}'");
+  } else if (!code.match(/^(\d{3})$/)) {
+    res.status(406).jsonp("Not Acceptable");
+  } else if (!router.db.get(code).value()) {
+    res.status(501).jsonp("Not Implemented");
+  } else {
+    res.status(+code).jsonp(res.locals.data.code);
+  }
+};
+
 server.get("/", (_req, res) => res.jsonp(router.db.value()));
 server.use(router);
 server.listen(PORT, () => {
