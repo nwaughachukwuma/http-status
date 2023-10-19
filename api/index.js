67b98f2e989463be0d06d4df4aadf691b3e6124a
Chukwuma Nwaugha <nwaughac@gmail.com>
2023-10-18 17:56:12 -0700

    use absolute path to db.json

--- api/index.js
+++ api/index.js
@@ -1,7 +1,9 @@
 const jsonServer = require("json-server");
+const path = require("node:path");
 
 const server = jsonServer.create();
-const router = jsonServer.router(require("../db.json"));
+const router = jsonServer.router(path.join(__dirname, "../db.json"));
+// const router = jsonServer.router(require("../db.json"));
 const middleware = jsonServer.defaults({
   readOnly: true,
 });
