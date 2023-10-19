e92da15548847247f09fe0eaa63ef2debd3496a5
Chukwuma Nwaugha <nwaughac@gmail.com>
2023-10-18 17:22:57 -0700

    set the server to readonly

--- index.js
+++ index.js
@@ -2,7 +2,9 @@ const jsonServer = require("json-server");
 
 const server = jsonServer.create();
 const router = jsonServer.router("db.json");
-const middleware = jsonServer.defaults();
+const middleware = jsonServer.defaults({
+  readOnly: true,
+});
 
 const PORT = process.env.PORT || 3000;
 
